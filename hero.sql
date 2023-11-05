1f28ebbc5fc078396789d1d78f81611beb39edd0
Brian <briantolentino311@gmail.com>
2023-11-04 19:56:46 -1200

    feat/update-delete-inactive

--- hero.sql
+++ hero.sql
@@ -159,3 +159,12 @@ VALUES
 	Set the column Price data type to Decimal with 2 decimal points.*/
     ALTER TABLE Item 
     ADD item_price DECIMAL(10, 2);
+
+    /*Update the state of hero 1 to inactive.
+	Delete the item associated with hero 1.*/
+    UPDATE Hero
+    SET status = 'inactive'
+    WHERE hero_id = 1;
+   
+    DELETE FROM Item 
+    WHERE hero_id = 1;
