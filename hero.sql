9a5c4de3e90bab85bb10a3297ef2fefa08a26178
Brian <briantolentino311@gmail.com>
2023-11-04 19:58:15 -1200

    feat/select-active-players

--- hero.sql
+++ hero.sql
@@ -168,3 +168,10 @@ VALUES
    
     DELETE FROM Item 
     WHERE hero_id = 1;
+
+    /*List the player names and their corresponding hero names that are actively in
+	use during gameplay, excluding those with inactive heroes.*/
+    SELECT p.player_name, h.hero_name
+    FROM Player p
+    JOIN Hero h ON p.hero_id = h.hero_id
+    WHERE p.player_experience > 0;
